ad6e972a976568b25562ad842614e659766e8feb
Scott Prue <sprue.dev@gmail.com>
2021-11-30 00:20:57 -0700

    fix: cleanup lint

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,11 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+importScripts('/__/firebase/9.2.0/firebase-app-compat.js')
+importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js')
+importScripts('/__/firebase/init.js')
 
-const messaging = firebase.messaging();
+const messaging = firebase.messaging()
 
 /**
  * Here is is the code snippet to initialize Firebase Messaging in the Service
@@ -13,8 +13,8 @@ const messaging = firebase.messaging();
  // Give the service worker access to Firebase Messaging.
  // Note that you can only use Firebase Messaging here. Other Firebase libraries
  // are not available in the service worker.
- importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
- importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js')
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js')
  // Initialize the Firebase app in the service worker by passing in
  // your app's Firebase config object.
  // https://firebase.google.com/docs/web/setup#config-object
@@ -27,10 +27,10 @@ const messaging = firebase.messaging();
    messagingSenderId: 'sender-id',
    appId: 'app-id',
    measurementId: 'G-measurement-id',
- });
+ })
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
- const messaging = firebase.messaging();
+ const messaging = firebase.messaging()
  **/
 
 
@@ -58,4 +58,4 @@ messaging.onBackgroundMessage(function(payload) {
     notificationTitle,
     notificationOptions
   )
-});
+})
